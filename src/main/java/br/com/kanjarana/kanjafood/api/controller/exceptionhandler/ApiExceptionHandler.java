f4182c75810652464aadc474f4c45ffb78614ee1
Kanjarana <kanjarana10@gmail.com>
2023-03-20 21:20:56 -0300

    8.19 Padronização da resposta de problemas com RFC 7807

---
+++ src/main/java/br/com/kanjarana/kanjafood/api/controller/exceptionhandler/ApiExceptionHandler.java
@@ -0,0 +1,120 @@
+package br.com.kanjarana.kanjafood.api.controller.exceptionhandler;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import br.com.kanjarana.kanjafood.domain.exception.EntidadeEmUsoException;
+import br.com.kanjarana.kanjafood.domain.exception.EntidadeNaoEncontradaException;
+import br.com.kanjarana.kanjafood.domain.exception.NegocioException;
+
+@ControllerAdvice
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler{
+
+	@ExceptionHandler(EntidadeNaoEncontradaException.class)
+	public ResponseEntity<?> handleEntidadeNaoEncontradaException(EntidadeNaoEncontradaException ex,
+			WebRequest request) {
+		
+		HttpStatus status = HttpStatus.NOT_FOUND;		
+		String detail = ex.getMessage();		
+		ProblemType problemType = ProblemType.ENTIDADE_NAO_ENCONTRADA;
+		
+		Problem problem = createProblemBuilder(status, problemType, detail).build();
+		
+//		Problem problem = Problem.builder()				
+//				.status(status.value())
+//				.type("https://kanjafood.com.br/entidade-nao-encontrada")
+//				.title("Entidade não encontrada")
+//				.detail(detail)
+//				.build();
+
+		
+		return handleExceptionInternal(ex, problem, new HttpHeaders(), 
+				status, request);
+		
+//		Problema problema = Problema.builder()
+//				.dataHora(LocalDateTime.now())
+//				.mensagem(e.getMessage()).build();
+//		
+//		return ResponseEntity.status(HttpStatus.NOT_FOUND)
+//				.body(problema);
+	}
+	
+	@ExceptionHandler(EntidadeEmUsoException.class)
+	public ResponseEntity<?> handleEntidadeEmUsoException(EntidadeEmUsoException ex,
+			WebRequest request) {
+		
+		HttpStatus status = HttpStatus.CONFLICT;		
+		String detail = ex.getMessage();		
+		ProblemType problemType = ProblemType.ENTIDADE_EM_USO;
+		
+		Problem problem = createProblemBuilder(status, problemType, detail).build();
+		
+		return handleExceptionInternal(ex, problem, new HttpHeaders(), 
+				status, request);
+	}
+	
+	@ExceptionHandler(NegocioException.class)
+	public ResponseEntity<?> tratarNegocioException(NegocioException ex,
+			WebRequest request) {
+		
+		HttpStatus status = HttpStatus.BAD_REQUEST;		
+		String detail = ex.getMessage();		
+		ProblemType problemType = ProblemType.ERRO_NEGOCIO;
+		
+		Problem problem = createProblemBuilder(status, problemType, detail).build();
+		
+		return handleExceptionInternal(ex, problem, new HttpHeaders(), 
+				status, request);
+	}
+	
+	
+	@Override
+	protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, 
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+		if (body == null) {
+			body = Problem.builder()
+					.title(status.getReasonPhrase())
+					.status(status.value())
+					.build();
+		}
+		else if (body instanceof String) {
+			body = Problem.builder()
+					.title((String) body)
+					.status(status.value())
+					.build();
+		}
+		
+		return super.handleExceptionInternal(ex, body, headers, status, request);
+	}
+	
+	
+	private Problem.ProblemBuilder createProblemBuilder(HttpStatus status,
+			ProblemType problemType, String detail) {
+		return Problem.builder()
+				.status(status.value())
+				.type(problemType.getUri())
+				.title(problemType.getTitle())
+				.detail(detail)
+				;
+	}
+	
+	/*
+	@ExceptionHandler(HttpMediaTypeNotSupportedException.class)
+	public ResponseEntity<?> tratarHttpMediaTypeNotSupportedException() {
+		Problema problema = Problema.builder()
+				.dataHora(LocalDateTime.now())
+				.mensagem("O tipo de mídia não é aceito.").build();
+		
+		return ResponseEntity.status(HttpStatus.UNSUPPORTED_MEDIA_TYPE)
+				.body(problema);
+	}
+	*/
+	
+	
+}
